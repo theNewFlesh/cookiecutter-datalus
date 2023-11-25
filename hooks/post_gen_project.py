2fbb0de6343e1b01817ac6e5cfe4d1960faef1a8
Alex Braun <alexander.g.braun@gmail.com>
2023-11-25 13:40:39 -0500

    WIP

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -20,23 +20,23 @@ def main():
     git_host = config['git_host']
 
     if rtype == 'library':
-        shutil.rmtree('helm', ignore_errors=True)
-        shutil.rmtree('python/' + repo_ + '/server', ignore_errors=True)
+        shutil.rmtree('helm')
+        shutil.rmtree('python/' + repo_ + '/server')
         os.remove('sphinx/server.rst')
 
     if rtype != 'dash':
-        shutil.rmtree('artifacts', ignore_errors=True)
-        shutil.rmtree('templates', ignore_errors=True)
+        shutil.rmtree('artifacts')
+        shutil.rmtree('templates')
         os.remove('python/conftest.py')
 
     if cli == 'no':
         os.remove('python/' + repo + '/command.py')
 
     if sphinx == 'no':
-        shutil.rmtree('sphinx', ignore_errors=True)
+        shutil.rmtree('sphinx')
     
     if mkdocs == 'no':
-        shutil.rmtree('mkdocs', ignore_errors=True)
+        shutil.rmtree('mkdocs')
 
     if helm == 'no':
         shutil.rmtree('helm', ignore_errors=True)
